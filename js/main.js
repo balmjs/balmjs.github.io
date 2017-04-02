0b7ea120d0769a1c103dc43977b9f36304fe609c
Elf-mousE <elf-mouse@elf-mouses-mbp.home>
2017-04-02 17:50:33 +0800

    new version for balm@0.6.0

--- js/main.js
+++ js/main.js
@@ -17,6 +17,23 @@
       console.log(key + ': ' + value);
     }
   };
+
+  var eventHandler = function(e) {
+    var el = e.target;
+    if (!el.classList.contains('active')) {
+      var type = el.dataset.type.toLowerCase();
+      el.parentNode.querySelector('li.active').classList.remove('active');
+      el.classList.add('active');
+      el.parentNode.parentNode.querySelector('pre.active').classList.remove('active');
+      document.getElementById(type + '-code').classList.add('active');
+    }
+  };
+
+  var tabs = document.querySelectorAll('.tabs');
+  tabs.forEach(function(tab) {
+    tab.addEventListener('click', eventHandler);
+  });
+
 })(window);
 
 console.info('Welcome to BalmJS, please select your project demo;)');
