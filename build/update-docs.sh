03751d92cd54ba37a8ddc83862639f298353db0f
Elf-mousE <ifmouz@gmail.com>
2018-01-15 21:02:10 +0800

    update

--- build/update-docs.sh
+++ build/update-docs.sh
@@ -4,4 +4,4 @@ cd dist
 git init
 git add -A
 git commit -m 'update official site'
-git push -f git@github.com:balmjs/balmjs.github.io.git dev:master
+git push -f git@github.com:balmjs/balmjs.github.io.git master
