8a05be259d58bde5cfa4cfc4aba29bb9ed5ab74c
Elf-mousE <ifmouz@gmail.com>
2019-06-01 10:36:43 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
 const balm = require('balm');
 const config = require('./config/balmrc');
-// require('./config/tasks'); // NOTE: integrated to balm@1.4.0
 
 balm.config = config;
-// NOTE: integrated to balm@1.4.0
-// if (balm.config.isProd) {
-//   balm.afterTask = 'generate-service-worker';
-// }
 
 balm.go(mix => {
+  // mix.copy('node_modules/prismjs/themes/prism-tomorrow.css', 'src/styles', {
+  //   basename: '_prism-tomorrow'
+  // });
+
   mix.copy(
     'node_modules/workbox-sw/build/workbox-sw.js',
     balm.config.isProd ? balm.config.roots.target : balm.config.roots.tmp
@@ -21,7 +20,7 @@ balm.go(mix => {
     mix.copy('archive.zip', 'dist', {
       basename: 'balm-example'
     });
-  }
 
-  mix.injectManifest();
+    mix.injectManifest();
+  }
 });
