a01a1d6eccec78ec6f945454c133c99479eb1057
Elf-mousE <ifmouz@gmail.com>
2019-11-20 11:24:58 +0800

    update version

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,7 +3,7 @@ importScripts('workbox-sw.js');
 // Configure Cache Names
 workbox.core.setCacheNameDetails({
   prefix: 'balm',
-  suffix: 'v20190711',
+  suffix: 'v20191120',
   precache: 'app-cache',
   runtime: 'app-runtime'
 });
